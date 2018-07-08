fadf01cac0f84728c02f36be50d541dc1b4c1af7
Srebalaji Thirumalai <srebalaji@outlook.com>
2018-07-09 00:12:17 +0530

    Cleanup code

--- day21/bellman_ford.rb
+++ day21/bellman_ford.rb
@@ -1,5 +1,12 @@
 # Bellman Ford
 
+class Integer
+  N_BYTES = [42].pack('i').size
+  N_BITS = N_BYTES * 16
+  MAX = 2 ** (N_BITS - 2) - 1
+  MIN = -MAX - 1
+end
+
 class Node
 	attr_accessor :data, :nodes, :path
 
@@ -29,36 +36,30 @@ c.nodes << [d, 4]
 d.nodes << [e, 4]
 b.nodes << [c, -3]
 
+@queue[a] = 0
+@queue[b] = Integer::MAX
+@queue[c] = Integer::MAX
+@queue[d] = Integer::MAX
+@queue[e] = Integer::MAX
+
+
+@queue_temp = [a,b,c,d,e]
 
 def traverse_node(node)
-	# p @queue.keys.map(&:data)
-	# p @queue.values
-	# p @queue_temp.map(&:data)
-	p @graph.map(&:path)
-	p "--------"
-	
 	if node.nil?
 		@inc = 0
 		return
 	end
 
 	current_node_weight = @queue[node]
-	# @queue.delete(node)
 
 	node.nodes.each do |element|
 		selected_element = element[0]
 		weight = element[1]
 
-
-		if @queue[selected_element].nil?
+		if @queue[selected_element] > current_node_weight + weight && (current_node_weight + weight) >= 0
 			@queue[selected_element] = current_node_weight + weight
-			@queue_temp << selected_element
 			selected_element.path = current_node_weight + weight
-		else
-			if @queue[selected_element] > current_node_weight + weight && (current_node_weight + weight) >= 0
-				@queue[selected_element] = current_node_weight + weight
-				selected_element.path = current_node_weight + weight
-			end
 		end
 	end
 
@@ -66,10 +67,9 @@ def traverse_node(node)
 	traverse_node(@queue_temp[@inc])
 end
 
-@queue[a] = 0
-@queue_temp << a
+
 a.path = 0
-(1..4).each do 
+(1..@queue_temp.length-1).each do 
 	traverse_node(a)
 end
 
