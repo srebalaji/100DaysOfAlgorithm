f7d095ddff9518646e5858bdbe548fe4f41a0dd4
Srebalaji Thirumalai <srebalaji@outlook.com>
2018-07-12 00:07:37 +0530

    Optimise some code

--- day24/sieve_of_eratosthenes.rb
+++ day24/sieve_of_eratosthenes.rb
@@ -1,6 +1,6 @@
 # Sieve Of Eratosthenes
 
-@n = 15
+@n = 5
 
 @prime = []
 
@@ -18,23 +18,24 @@ def cross_number(number)
 	loop do
 		break if i > @n
 		@prime[i] = false
+		i += number
 	end
 end
 
 def get_next_prime
 	next_num = @prime_num + 1
-	
 	loop do
 		break if next_num > @n
+		break if @prime[next_num] == true
 		next_num += 1
 	end
 	next_num
 end
 
+
 while(@prime_num <= Math.sqrt(@n))
 	cross_number(@prime_num)
-
 	@prime_num = get_next_prime
 end
 
-p prime
+p @prime
